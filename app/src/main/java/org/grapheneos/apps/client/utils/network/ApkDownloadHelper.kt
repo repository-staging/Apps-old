317bb4d9cb52d89333c64942ba63cce45cc28b8d
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2021-10-22 15:13:40 +0000

    Move incremental change earlier for calculationSize

--- app/src/main/java/org/grapheneos/apps/client/utils/network/ApkDownloadHelper.kt
+++ app/src/main/java/org/grapheneos/apps/client/utils/network/ApkDownloadHelper.kt
@@ -56,13 +56,14 @@ class ApkDownloadHelper constructor(private val context: Context) {
 
                     completeProgress[fileName] = newProgress
                     completeProgress.forEach { (_, progress) ->
+                        calculationSize++
                         read += progress.read
                         total += progress.total
                         completed = completed && progress.taskCompleted
-                        calculationSize++
                     }
 
-                    val doneInPercent = if (calculationSize == size && total.toInt() != 0) (read * 100.0) / total else -1.0
+                    val shouldCompute = calculationSize == size && total.toInt() != 0
+                    val doneInPercent = if (shouldCompute) (read * 100.0) / total else -1.0
 
                     progressListener.invoke(
                         read,
