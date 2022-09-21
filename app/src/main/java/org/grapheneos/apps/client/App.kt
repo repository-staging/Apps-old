7bd7c465f41ea6d10f1e0aa79ce6c4214664ed8f
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2022-09-21 14:01:25 +0000

    Show snackbar as well on failed refresh when pending one exists

--- app/src/main/java/org/grapheneos/apps/client/App.kt
+++ app/src/main/java/org/grapheneos/apps/client/App.kt
@@ -331,7 +331,11 @@ class App : Application() {
     @RequiresPermission(Manifest.permission.INTERNET)
     fun refreshMetadata(force: Boolean = false, callback: (error: MetadataCallBack) -> Unit) {
         when {
-            isMetadataSyncing() -> refreshScope.cancel()
+            isMetadataSyncing() -> {
+                refreshScope.cancel()
+                callback.invoke(MetadataCallBack.SecurityError(Exception(
+                    App.getString(R.string.refreshInProgress))))
+            }
             packagesInfo.isNotEmpty() && !force -> return
             isDownloading -> {
                 callback.invoke(MetadataCallBack.SecurityError(Exception(
